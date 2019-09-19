bb49673b2d82138d5414491eec88c81ee3d59703
JeanCarl <JeanCarlButronMaster@gmail.com>
2019-09-18 21:13:38 -0500

    agrega maquetacion para administrador

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('online.layout.index');
+// });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('online.layout.index');
+    return view('admin.layout.index');
 });
