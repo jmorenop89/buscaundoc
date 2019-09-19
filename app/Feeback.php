f2f377145d64a6cd4888587d29807ac13eb1f627
Edder <yslaeder@hotmail.com>
2019-09-18 19:32:40 -0500

    feedback

--- app/Feeback.php
+++ app/Feeback.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Feeback extends Model
 {
     //
-    public $table = 'contenido';
+    public $table = 'feedback';
 
     protected $fillable = [
         'contenido', 'doctor_id', 'paciente_id',
