f09a1204f87760d2536eed2a375b2bf9b34058ae
Alberto Barreto <joalbcgit@gmail.com>
2019-09-18 20:50:19 -0500

    agregando el footer en layout

---
+++ resources/views/online/layout/footer.blade.php
@@ -0,0 +1,61 @@
+<footer>
+		<div class="container margin_60_35">
+			<div class="row">
+				<div class="col-lg-3 col-md-12">
+					<p>
+						<a href="index.html" title="Findoctor">
+							<img src="/online/img/logo.png" data-retina="true" alt="" width="163" height="36" class="img-fluid">
+						</a>
+					</p>
+				</div>
+				<div class="col-lg-3 col-md-4">
+					<h5>Abouts</h5>
+					<ul class="links">
+						<li><a href="#0">Acerca de nosotros</a></li>
+						<li><a href="blog.html">Blog</a></li>
+						<li><a href="#0">FAQ</a></li>
+						<li><a href="login.html">Iniciar Sesion</a></li>
+						<li><a href="register.html">Registrate</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-4">
+					<h5>Enlaces Utiles</h5>
+					<ul class="links">
+						<li><a href="#0">Doctores</a></li>
+						<li><a href="#0">Clinics</a></li>
+						<li><a href="#0">Especializacion</a></li>
+						<li><a href="#0">Unete como un Doctor</a></li>						
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-4">
+					<h5>Contact with Us</h5>
+					<ul class="contacts">
+						<li><a href="tel://61280932400"><i class="icon_mobile"></i> + 61 23 8093 3400</a></li>
+						<li><a href="mailto:info@findoctor.com"><i class="icon_mail_alt"></i> help@findoctor.com</a></li>
+					</ul>
+					<div class="follow_us">
+						<h5>Follow us</h5>
+						<ul>
+							<li><a href="#0"><i class="social_facebook"></i></a></li>
+							<li><a href="#0"><i class="social_twitter"></i></a></li>
+							<li><a href="#0"><i class="social_linkedin"></i></a></li>
+							<li><a href="#0"><i class="social_instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!--/row-->
+			<hr>
+			<div class="row">
+				<div class="col-md-8">
+					<ul id="additional_links">
+						<li><a href="#0">Terms and conditions</a></li>
+						<li><a href="#0">Privacy</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<div id="copy">© 2017 Findoctor</div>
+				</div>
+			</div>
+		</div>
+	</footer>
