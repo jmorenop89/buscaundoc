2788352dbbb02819103ebaaeee5d8617571aafc2
Jorge Briceno <jbriceno40@gmail.com>
2019-09-18 20:03:54 -0500

    especialidades parte 1

--- database/seeds/EspecialidadesSeeder.php
+++ database/seeds/EspecialidadesSeeder.php
@@ -12,5 +12,108 @@ class EspecialidadesSeeder extends Seeder
     public function run()
     {
         //
+        DB::table("especialidad")->delete();
+        DB::table("especialidad")->insert(
+            [
+            array(
+                'nombre'=>"Ginecólogo",
+            ),
+            array(
+                'nombre'=>"Traumatólogo y Ortopedista",
+            ),
+            array(
+                'nombre'=>"Dermatólogo",
+            ),
+            array(
+                'nombre'=>"Pediatra",
+            ),
+            array(
+                'nombre'=>"Cirujano general",
+            ),
+            array(
+                'nombre'=>"Gastroenterólogo",
+            ),
+            array(
+                'nombre'=>"Urólogo",
+            ),
+            array(
+                'nombre'=>"Oftalmólogo",
+            ),
+            array(
+                'nombre'=>"Neurólogo",
+            ),
+            array(
+                'nombre'=>"Otorrino",
+            ),
+            array(
+                'nombre'=>"Psiquiatra",
+            ),
+            array(
+                'nombre'=>"Endocrinólogo",
+            ),
+            array(
+                'nombre'=>"Neumólogo",
+            ),
+            array(
+                'nombre'=>"Cardiólogo",
+            ),
+            array(
+                'nombre'=>"Neurocirujano",
+            ),
+            array(
+                'nombre'=>"Internista",
+            ),
+            array(
+                'nombre'=>"Psicólogo",
+            ),
+            array(
+                'nombre'=>"Reumatólogo",
+            ),
+            array(
+                'nombre'=>"Médico general",
+            ),
+            array(
+                'nombre'=>"Cirujano plástico",
+            ),
+            array(
+                'nombre'=>"Nefrólogo",
+            ),
+            array(
+                'nombre'=>"Cirujano cardiovascular y torácico",
+            ),
+            array(
+                'nombre'=>"Cirujano maxilofacial",
+            ),
+            array(
+                'nombre'=>"Oncólogo",
+            ),
+            array(
+                'nombre'=>"Hematólogo",
+            ),
+            array(
+                'nombre'=>"Nutricionista",
+            ),
+            array(
+                'nombre'=>"Especialista en Medicina Física y Rehabilitación",
+            ),
+            array(
+                'nombre'=>"Radiólogo",
+            ),
+            array(
+                'nombre'=>"Dentista",
+            ),
+            array(
+                'nombre'=>"Alergista",
+            ),
+            array(
+                'nombre'=>"Geriatra",
+            ),
+            array(
+                'nombre'=>"Cirujano pediátrico",
+            ),
+            ]
+            
+        );
     }
+   
 }
