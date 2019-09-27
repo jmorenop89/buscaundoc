61a3b575329d89f0086efffa2cd3006555e17940
Edder <yslaeder@hotmail.com>
2019-09-26 19:02:37 -0500

    primeros pasos para el mail

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,11 +1,10 @@
-@extends('layouts.app')
+@extends('template.layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="bg_color_2">
+    <div class="justify-content-center">
+        <div class="container margin_60_35">
+            <div class="login">
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
