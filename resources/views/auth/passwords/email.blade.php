6ec5a23094930df51b01086a347de7f204ae1e06
Edder <yslaeder@hotmail.com>
2019-10-01 11:58:17 -0500

    estado de envio de contraseña

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -14,9 +14,11 @@
                         @csrf
                         <div class="form-group">
                             <label for="email" class="col-form-label">{{ __('E-Mail Address') }}</label>
-                            @session('status')
-                            <p>bien</p>
-                            @endsession
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
