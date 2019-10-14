96c3143eae9d1d7eb1b925eba515109ff6fa6da7
Jorge Briceno <jbriceno40@gmail.com>
2019-10-14 11:08:34 -0500

    modificando head de doctor y paciente

--- resources/views/online/log-doc/index.blade.php
+++ resources/views/online/log-doc/index.blade.php
@@ -137,9 +137,8 @@
 		<div id="breadcrumb">
 			<div class="container">
 				<ul>
-					<li><a href="#">Home</a></li>
-					<li><a href="#">Category</a></li>
-					<li>Page active</li>
+					<li><a href="/">Inicio</a></li>
+					<li>Doctor</li>
 				</ul>
 			</div>
 		</div>
